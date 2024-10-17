861f04c620022c261bf1ef48ac6d5d5c1178f946
Khizarali07 <khizarali.cs@gmail.com>
2024-10-17 05:54:31 -0700

    styleadded

--- src/assets/ActivityCard.jsx
+++ src/assets/ActivityCard.jsx
@@ -57,7 +57,7 @@ function ActivityCard({
 
   return (
     <div
-      className="card text-center my-3"
+      className="container card my-3"
       style={{
         height: "330px",
         boxShadow: "10px 10px 5px #aaaaaa",
@@ -81,57 +81,59 @@ function ActivityCard({
           {activityDescription}
         </h6>
         <p className="card-text mt-3">Notes : {notes}</p>
-        <button
-          className="me-3 bg-white my-3"
-          data-bs-toggle="modal"
-          data-bs-target={`#exampleModal-${id}`}
-          style={{ border: "none" }}
-        >
-          <i
-            className="fa-solid fa-pen-to-square text-dark"
-            style={{ fontSize: "20px" }}
-          ></i>
-        </button>
+        <div className="text-center">
+          <button
+            className="me-3 bg-white my-3"
+            data-bs-toggle="modal"
+            data-bs-target={`#exampleModal-${id}`}
+            style={{ border: "none" }}
+          >
+            <i
+              className="fa-solid fa-pen-to-square text-dark"
+              style={{ fontSize: "20px" }}
+            ></i>
+          </button>
 
-        <Updateactivity
-          assignTo={assignTo}
-          activityDescription={activityDescription}
-          notes={notes}
-          dateActivity={dateActivity}
-          dateFollowUp={dateFollowUp}
-          reason={reason}
-          id={id}
-          LinkID={linkID}
-          fetchActivity={fetchActivity}
-        />
+          <Updateactivity
+            assignTo={assignTo}
+            activityDescription={activityDescription}
+            notes={notes}
+            dateActivity={dateActivity}
+            dateFollowUp={dateFollowUp}
+            reason={reason}
+            id={id}
+            LinkID={linkID}
+            fetchActivity={fetchActivity}
+          />
 
-        <button
-          type="button"
-          className="bg-white"
-          data-bs-toggle="modal"
-          data-bs-target={`#modal-delete-${id}`}
-          style={{ border: "none" }}
-        >
-          <i
-            className="fa-solid fa-trash text-dark"
-            style={{ fontSize: "20px" }}
-          ></i>
-        </button>
+          <button
+            type="button"
+            className="bg-white"
+            data-bs-toggle="modal"
+            data-bs-target={`#modal-delete-${id}`}
+            style={{ border: "none" }}
+          >
+            <i
+              className="fa-solid fa-trash text-dark"
+              style={{ fontSize: "20px" }}
+            ></i>
+          </button>
 
-        <Deleteactivity id={id} fetchActivity={fetchActivity} />
+          <Deleteactivity id={id} fetchActivity={fetchActivity} />
 
-        <p className="my-2">
-          <strong>Created date : </strong>
-          {formatDate(dateCreated)}
-        </p>
-        <p className="my-2">
-          <strong>Last Date : </strong>
-          {formatDate(dateActivity)}
-        </p>
-        <p className="my-2">
-          <strong>Follow Up : </strong>
-          {formatDate(dateFollowUp)}
-        </p>
+          <p className="my-2">
+            <strong>Created date : </strong>
+            {formatDate(dateCreated)}
+          </p>
+          <p className="my-2">
+            <strong>Last Date : </strong>
+            {formatDate(dateActivity)}
+          </p>
+          <p className="my-2">
+            <strong>Follow Up : </strong>
+            {formatDate(dateFollowUp)}
+          </p>
+        </div>
       </div>
     </div>
   );
